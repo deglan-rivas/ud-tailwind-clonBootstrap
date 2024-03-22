7708445f4d21c4b6dba42e988ec831522a750513
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-22 15:52:31 -0500

    [ADD] outline buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = () => {
     <>
       <p>los button ya incluyen el cursor-pointer del hover</p>
 
-      <div>
+      <div className="mb-2">
         <button className="bg-blue-500 text-white rounded-md px-2 py-1 mr-2 hover:bg-blue-600">
           Primary
         </button>
@@ -33,6 +33,40 @@ const Button = () => {
         </button>
 
         <button className="bg-black text-white border border-white rounded-md px-2 py-1 mr-2 hover:bg-gray-800">
+          Dark
+        </button>
+      </div>
+
+      <div>
+        <button className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 mr-2 hover:bg-blue-500 hover:text-white">
+          Primary
+        </button>
+
+        <button className="text-gray-500 border border-gray-500 rounded-md px-2 py-1 mr-2 hover:bg-gray-500 hover:text-white">
+          Secondary
+        </button>
+
+        <button className="text-green-500 border border-green-500 rounded-md px-2 py-1 mr-2 hover:bg-green-500 hover:text-white">
+          Success
+        </button>
+
+        <button className="text-red-500 border border-red-500 rounded-md px-2 py-1 mr-2 hover:bg-red-500 hover:text-white">
+          Danger
+        </button>
+
+        <button className="text-yellow-500 border border-yellow-500 rounded-md px-2 py-1 mr-2 hover:bg-yellow-500 hover:text-white">
+          Warning
+        </button>
+
+        <button className="text-cyan-500 border border-cyan-500 rounded-md px-2 py-1 mr-2 hover:bg-cyan-500 hover:text-white">
+          Info
+        </button>
+
+        <button className="text-black border border-black rounded-md px-2 py-1 mr-2 hover:bg-black hover:text-white">
+          Light
+        </button>
+
+        <button className="text-white border border-white rounded-md px-2 py-1 mr-2 hover:bg-black hover:text-white">
           Dark
         </button>
       </div>
