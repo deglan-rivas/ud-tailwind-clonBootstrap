b8addabbe58bddb185c90499f228e6d3109222b3
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-23 14:49:43 -0500

    [ADD] customed checkbox

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,6 +1,7 @@
 const ButtonGroup = () => {
   return (
     <div className="bg-gray-300 p-8">
+      <p>La class peer al input es la clave para enlazar el estado del checkbox con label sin usar JS. Input debe estar antes. Notar que eslintrc.cjs del vite ya corrige el for por un htmlFor. También hay que separar cada uno en un div propio de input y label. Usar el flex o inline-block para alinear</p>
       <div className="mb-2">
         <button 
           className="bg-blue-600 text-white rounded-l px-4 py-2 hover:bg-blue-700"
@@ -107,6 +108,39 @@ const ButtonGroup = () => {
         >
           Right
         </button>
+      </div>
+
+      <div className="mb-2 flex">
+        {/* <div className="inline-block"> */}
+        <div>
+          <input type="checkbox" className="peer hidden" id="c-left" />
+          <label
+            className="border-l border-y border-blue-600 bg-white text-blue-600 px-4 py-2 rounded-l peer-checked:bg-blue-600 peer-checked:text-white"
+            htmlFor="c-left"
+            >
+            Left
+          </label>
+        </div>
+        {/* <div className="inline-block"> */}
+        <div>
+          <input type="checkbox" className="peer hidden" id="c-middle" />
+          <label
+            className="border-y border-blue-600 bg-white text-blue-600 px-4 py-2 hover:bg-blue-600 hover:text-white peer-checked:bg-blue-600 peer-checked:text-white"
+            htmlFor="c-middle"
+            >
+            Middle
+          </label>
+          </div>
+        {/* <div className="inline-block"> */}
+        <div>
+          <input type="checkbox" className="peer hidden" id="c-right" />
+          <label
+            className="border-r border-y border-blue-600 bg-white text-blue-600 px-4 py-2 rounded-r hover:bg-blue-600 hover:text-white peer-checked:bg-blue-600 peer-checked:text-white"
+            htmlFor="c-right"
+            >
+            Right
+          </label>
+        </div>
       </div>
     </div>
   )
