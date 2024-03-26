04e530aa2ddf87d4ae3eb9e766dff0e51ebefcc5
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-25 23:58:21 -0500

    [ADD] featured and card footer

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ const Card = () => {
           This is some text within a card body
         </div>
 
-        <div className="w-72 border border-gray-200 px-4 pt-3 pb-3">
+        <div className="w-72 border border-gray-200 px-4 pt-3 pb-3 rounded-md mb-4">
           <h5 className="text-xl font-medium">
             Card title
           </h5>
@@ -14,7 +14,7 @@ const Card = () => {
             Card subtitle
           </h6>
           <p className="py-2">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          Some quick example text to build on the card title and make up the bulk of the card`s content.
           </p>
           <a 
             href=""
@@ -29,6 +29,26 @@ const Card = () => {
             Another link
           </a>
         </div>
+
+        <ul className="border border-gray-300 divide-y rounded-md overflow-hidden w-60 mb-4">
+          <li className="py-2 px-3">A item</li>
+          <li className="py-2 px-3">A second item</li>
+          <li className="py-2 px-3">A third item</li>
+        </ul>
+
+        <ul className="border border-gray-300 divide-y rounded-md overflow-hidden w-60 mb-4">
+          <li className="py-2 px-3 bg-red-50">Featured</li>
+          <li className="py-2 px-3">A item</li>
+          <li className="py-2 px-3">A second item</li>
+          <li className="py-2 px-3">A third item</li>
+        </ul>
+
+        <ul className="border border-gray-300 divide-y rounded-md overflow-hidden w-60 mb-4">
+          <li className="py-2 px-3">A item</li>
+          <li className="py-2 px-3">A second item</li>
+          <li className="py-2 px-3">A third item</li>
+          <li className="py-2 px-3 bg-gray-200">Card footer</li>
+        </ul>
       </div>  
     </>
   )
