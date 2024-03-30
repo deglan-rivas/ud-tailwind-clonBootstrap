d8e6e347898853202fed8ca7aefa58094bc092c0
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-30 14:06:59 -0500

    [ADD] bg contain

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -493,12 +493,35 @@ const Card = () => {
           </div>
         </div>
 
-        <p className="mb-4">el auto corta la imagen redimensionada sin deformar. El cover ajusta y, como tiene que cubrir todo, termina por deforma alguna dimensión. En esa dimensión se puede mover pues hay más contenido de lo que se ve o debería verse</p>
+        <p className="mb-4">el auto muestra la imagen sin deformar. El cover redimensiona hasta que alguna demensión se entalle y sobre en la otra. En esa dimensión se puede mover pues hay más contenido de lo que se ve. Contain solo hace el resize hasta que la priemra dimensión se entalle, combinar con el no-repeat o repeat-around</p>
         <div className="border border-gray-300 rounded-md divide-y w-72 overflow-hidden mb-4"> 
           {/* <div className="h-48 overflow-hidden">
             <img src="https://cdn.pixabay.com/photo/2023/06/28/19/05/beautiful-black-woman-8095034_1280.jpg" alt="test" />
           </div> */}
-          <div className="h-96 border border-gray-300 flex justify-center items-center bg-cover bg-center bg-[url('https://cdn.pixabay.com/photo/2023/12/14/00/20/alaska-8448009_1280.jpg')]" >
+          <div className="h-96 border border-gray-300 flex justify-center items-center bg-cover bg-left bg-[url('https://cdn.pixabay.com/photo/2023/12/14/00/20/alaska-8448009_1280.jpg')]">
+            <p className="text-white text-4xl select-none">Image cap</p>
+          </div>
+          <div className=" p-2">
+            <h5 className="text-lg font-medium py-2">
+              Card title
+            </h5>
+            <p className="mb-2">
+              Some quick example text to build on the card title and make up the bulk of the cards content.
+            </p>
+            <a 
+              href="#"
+              className="bg-blue-600 hover:bg-blue-800 text-white px-3 py-2 rounded-md inline-block"
+            >
+              Go somewhere
+            </a>
+          </div>
+        </div>
+
+        <div className="border border-gray-300 rounded-md divide-y w-72 overflow-hidden mb-4"> 
+          {/* <div className="h-48 overflow-hidden">
+            <img src="https://cdn.pixabay.com/photo/2023/06/28/19/05/beautiful-black-woman-8095034_1280.jpg" alt="test" />
+          </div> */}
+          <div className="h-96 border border-gray-300 flex justify-center items-center bg-contain bg-no-repeat bg-center bg-[url('https://cdn.pixabay.com/photo/2023/12/14/00/20/alaska-8448009_1280.jpg')]">
             <p className="text-white text-4xl select-none">Image cap</p>
           </div>
           <div className=" p-2">
