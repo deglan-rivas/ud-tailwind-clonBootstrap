9ce6f97c522ad30ceb58c77c02329f17023a31e9
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-03-31 10:39:44 -0500

    [ADD] flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import S1 from "./HOC - topics/s1-intro"
+import S4 from "./HOC - topics/s4-flex"
 
 function App() {
   return(
     <>
       <h1 className="mb-4">hola mundo</h1>
-      <S1/>
+      {/* <S1/> */}
+      <S4/>
     </>
   )
 }
