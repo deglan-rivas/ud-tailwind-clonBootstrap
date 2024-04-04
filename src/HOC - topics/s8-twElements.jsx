b60fd4a71165fb1500b623d44019fa649cd1095f
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-03 19:17:38 -0500

    [ADD] collapse personal

--- src/HOC - topics/s8-twElements.jsx
+++ src/HOC - topics/s8-twElements.jsx
@@ -1,13 +1,15 @@
 import Accordion from "../components/twElements/Accordion";
 import Accordion2 from "../components/twElements/Accordion2";
 import Carrusel from "../components/twElements/Carousel";
+import CollapseBasicExample from "../components/twElements/Collapse";
 
 const S8 = () => {
   return (
     <>
       {/* <Accordion/> */}
       {/* <Accordion2/> */}
-      <Carrusel/>
+      {/* <Carrusel/> */}
+      <CollapseBasicExample/>
     </>
   )
 }
