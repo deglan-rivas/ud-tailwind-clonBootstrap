1c3cb42c7358dc40a1e7fc29ffd24ad15cfdd237
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-08 15:54:27 -0500

    [ADD] svg

--- src/HOC - topics/s10-tailwindDocs.jsx
+++ src/HOC - topics/s10-tailwindDocs.jsx
@@ -1,12 +1,14 @@
-import Position from "../components/tailwindDocs/Position"
-import Position2 from "../components/tailwindDocs/Position2"
-import Position3 from "../components/tailwindDocs/Position3"
+import Position from "../components/tailwindDocs/layout/Position"
+import Position2 from "../components/tailwindDocs/layout/Position2"
+import Position3 from "../components/tailwindDocs/layout/Position3"
+import Svg from "../components/tailwindDocs/svg/Svg"
 
 const S10 = () => {
   return (
     // <Position/>
     // <Position2/>
-    <Position3/>
+    // <Position3/>
+    <Svg/>
   )
 }
 
