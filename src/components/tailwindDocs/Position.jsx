5d4c61d695b35162d07642381dff8567e3f84173
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-07 23:41:52 -0500

    [UPDATE] relative and absolute

--- src/components/tailwindDocs/Position.jsx
+++ src/components/tailwindDocs/Position.jsx
@@ -1,14 +1,14 @@
 /**
- * buenas explicaciones de 
+ * buenas explicaciones de gotazu dev y jonyl, keyquotes también y fue más completo pero vendió humo con padre relative xd fixed es para que se quede fixed con el scroll y sticky es un fixed, pero cuando no comienza desde el borde xd
  * 
  */
 
 const Position = () => {
   return (
     <div className="relative">
-      <div className="bg-red-400 w-96 h-96 relative flex justify-center items-center">
+      <div className="bg-red-400 w-96 h-96 relative flex justify-center items-center top-0">
         <div>ga</div>
-        <div className="w-8 h-8 rounded-full bg-yellow-400 absolute right-8 bottom-8">
+        <div className="w-8 h-8 rounded-full bg-yellow-400 absolute right-8 bottom-8 flex items-center justify-center">
           Raaa
         </div>
       </div>
