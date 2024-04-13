375e61bf6fb95e1142c780472cca6c4d4ba95b75
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-12 23:59:13 -0500

    [ADD] footer 2

--- src/components/clonMeta/Footer.jsx
+++ src/components/clonMeta/Footer.jsx
@@ -338,9 +338,54 @@ const Footer = () => {
 
       <div className='px-8 py-20'>
         <div className='grid grid-cols-3'>
-          <a href='#' className='underline'>
-            United States (English)
-          </a>
+          <div className='col-span-1'>
+            <a href='#' className='underline'>
+              United States (English)
+            </a>
+          </div>
+
+          <div className='col-span-2 space-y-3 text-xs pt-3'>
+            <p className='uppercase'>
+              Meta quest
+            </p>
+            <p>
+              Meta Quest: *Parents:* Important guidance & safety warnings for children’s use here. Meta accounts for ages 10+ on Meta Quest 2 and 3; all other Meta Quest headsets for ages 13+. Certain apps, games and experiences may be suitable for a more mature audience. META QUEST FEATURES, FUNCTIONALITY, AND CONTENT NOTICE: Features, functionality and content are subject to change or withdrawal at any time, may not be available in all areas or languages or may be restricted; may require enabled software or service activation, and additional terms, conditions and/or charges may apply.
+            </p>
+
+            <p>
+              Certain apps and experiences will be available by winter 2023.
+            </p>
+
+            <p className=''>
+              META QUEST IMPORTANT SAFETY NOTICE 
+              <a href="#" className='text-blue-600 hover:text-blue-500 pl-1 underline'>
+                https://www.meta.com/quest/quest-2-facial-interface-recall/.
+              </a>
+            </p>
+
+            <p className=''>
+              Financing Options. You may be offered financing options for your Meta purchases. Learn more
+              <a href="#" className='text-blue-600 hover:text-blue-500 pl-1 underline'>
+                here.
+              </a>
+            </p>
+
+            <p>
+              *Based on the graphic performance of the Qualcomm Snapdragon XR2 Gen 2 vs XR2 Gen 1 on Meta Quest 2
+            </p>
+
+            <p>
+              RAY-BAN | META SMART GLASSES
+            </p>
+
+            <p>
+              Ray-Ban Meta smart glasses require a mobile phone with Android (location services enabled) or iOS operating system, wireless internet access, and account registration. Not compatible with all smartphones. Ray-Ban Meta smart glasses only available in select countries. Features, functionality and content are subject to change or withdrawal at any time. Please visit https://www.meta.com/smart-glasses/#faq for the most up-to-date information. For ages 13 and up only. This product may interfere with personal medical devices. Requires updates to be installed from time to time, including prior to first use. While using Ray-Ban Meta smart glasses, it is always the user’s responsibility to comply with any local laws and regulations, especially those relating to privacy and the use of recording devices. Additional account registration, terms and fees may apply.
+            </p>
+
+            <p>
+              ©2024 Meta.
+            </p>
+          </div>
         </div>
       </div>
     </footer>
